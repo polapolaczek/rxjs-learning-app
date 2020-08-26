443270ea5bbf69a81f219abac839a3049e09b9a8
Pola Polaczek <pola.polaczek@softwaremind.com>
2020-08-26 16:46:06 +0200

    Updates

---
+++ precache-manifest.0a124fa077f18483ada1a84c3f82164a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeac34df527818c5c7880eb34f5949d3",
+    "url": "/rxjs-learning-app/index.html"
+  },
+  {
+    "revision": "2fe7326e2347eb708a7b",
+    "url": "/rxjs-learning-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16c7ee1599fc55d60ea3",
+    "url": "/rxjs-learning-app/static/js/2.1121f578.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/rxjs-learning-app/static/js/2.1121f578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe7326e2347eb708a7b",
+    "url": "/rxjs-learning-app/static/js/main.a75bae07.chunk.js"
+  },
+  {
+    "revision": "29a60e5ea6c4b0b75f48",
+    "url": "/rxjs-learning-app/static/js/runtime-main.e1a55127.js"
+  }
+]);
